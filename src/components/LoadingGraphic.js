672335da6dccd0a2aa12bb0fb4309d37ad50ebb2
ted <ted@warpedpuppy.com>
2020-11-10 12:02:24 -0500

    contact form

--- src/components/LoadingGraphic.js
+++ src/components/LoadingGraphic.js
@@ -1,19 +1,26 @@
 import React from 'react'
-
+import './LoadingGraphic.css'
 export default function LoadingGraphic(props) {
     let style = {
         width: `${props.w}px`,
         height: `${props.h}px`,
         margin: `0 auto`,
         display: props.loaded ? 'none' : 'block',
-        border: "1px solid black",
-        backgroundColor: "blue",
+        // border: "1px solid black",
+       // backgroundColor: "blue",
 
         
     }
     return (
         <div style={style} className={`loading-graphic ${props.loaded}`}>
-            <h1>loading. . . </h1>
+            <div className="loading-background">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="loading-words">loading. . . </div>
         </div>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,6 @@ export default class Contact extends Component {
 
     onSubmitHandler = (e)  => {
         e.preventDefault();
-        alert("boom")
     }
     render() {
         return (
@@ -14,14 +13,14 @@ export default class Contact extends Component {
                     <h3>Contact Gideon:</h3>
                     <Form onSubmit={this.onSubmitHandler}>
                         <Form.Group controlId="exampleForm.ControlInput1">
-                            <Form.Label>Email address</Form.Label>
+                            <Form.Label>email address:</Form.Label>
                             <Form.Control type="email" placeholder="name@example.com" />
                         </Form.Group>
                         <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Example textarea</Form.Label>
+                            <Form.Label>message:</Form.Label>
                             <Form.Control as="textarea" rows={3} />
                         </Form.Group>
-                        <Button type="submit" variant="outline-primary">Primary</Button>
+                        <Button type="submit" variant="outline-primary">send form</Button>
                     </Form>
                     <div className="results"></div>
                 </div>
