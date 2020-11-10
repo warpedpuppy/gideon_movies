47f059848a36190e3f8eb14514b0e68625bbcefd
ted <ted@warpedpuppy.com>
2020-11-10 13:09:45 -0500

    change background photo

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,7 @@ export default class NotFound extends Component {
     render() {
         return (
             <div>
-                
+                <h1>not found</h1>
             </div>
         )
     }
