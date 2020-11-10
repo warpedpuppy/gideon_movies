acbacd6cc24a4ec407b45bf5de91ab78aa11ddc5
ted <ted@warpedpuppy.com>
2020-11-10 13:40:01 -0500

    change background photo

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,26 +4,38 @@ import './Movie.css';
 // import Movie1 from './movies/Movie1'
 export default class Movie extends Component {
 
+    componentDidMount () {
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
     state = {
-        loaded: false
+        loaded: false,
+        width: 0,
+        height: 0
     }
     loadComplete = () => {
         this.props.setLoaded(true);
+        this.updateWindowDimensions();
     }
-
+    updateWindowDimensions = () => {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+      }
     render() {
        
         let loadedClass = this.props.loaded ? "loaded" : "not-loaded" ;
+        let style  = {
+            width: this.state.width > 768 ? this.props.movie.width : "95vw",
+            height: this.props.movie.height
+        }
         return (
             <div className="movie-shell">
                 <LoadingGraphic w={this.props.movie.width} h={this.props.movie.height} loaded={this.props.loaded} />
                 <div className={`${loadedClass} iFrameClass`}>
                 <iframe 
-               
+                style={style}
                 src={this.props.movie.src}  
                 title="movie 4" 
-                width={this.props.movie.width}  
-                height={this.props.movie.height}   
+                // width={this.props.movie.width}  
+                 height={this.props.movie.height}   
                 frameBorder="0" 
                 allow="autoplay; fullscreen" 
                 onLoad={() => this.loadComplete()}
