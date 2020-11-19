28ab7b7bdda268dc8785afcbe6b1f80914a938fd
ted <ted@warpedpuppy.com>
2020-11-19 12:52:38 -0500

    contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react'
 import { Form, Button} from 'react-bootstrap';
 import './Contact.css';
 export default class Contact extends Component {
-
-    onSubmitHandler = (e)  => {
+    state = {
+        message: "test"
+    }
+    onSubmitHandler = async (e)  => {
         e.preventDefault();
+        let email = e.target.email.value,
+            message = e.target.message.value,
+            checker = e.target.checker.value;
+    
+        if (email && message) {
+            let response = await fetch(`http://localhost:8000/api/gideon/`, {
+                body: JSON.stringify({ email, message, checker }),
+                headers: {
+                 'content-type': 'application/json',
+                },
+                method: "POST"
+            });
+            let responseJson = await response.json();
+            console.log(responseJson)
+       
+        } else {
+            this.setState({message: "all fields required"})
+        }
+
+      
     }
     render() {
         return (
@@ -15,15 +37,18 @@ export default class Contact extends Component {
                     <Form onSubmit={this.onSubmitHandler}>
                         <Form.Group controlId="exampleForm.ControlInput1">
                             <Form.Label>email address:</Form.Label>
-                            <Form.Control type="email" placeholder="name@example.com" />
+                            <Form.Control type="email" name="email" placeholder="name@example.com" required/>
                         </Form.Group>
                         <Form.Group controlId="exampleForm.ControlTextarea1">
                             <Form.Label>message:</Form.Label>
-                            <Form.Control as="textarea" rows={3} />
+                            <Form.Control as="textarea" name="message" rows={3} required />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control as="input" name="checker" />
                         </Form.Group>
                         <Button type="submit" variant="outline-primary">send form</Button>
                     </Form>
-                    <div className="results"></div>
+                    <div className="results">{this.state.message}</div>
                 </div>
             </section>
         )
