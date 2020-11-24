db38b44e03d8830963c8ad1cdc947fa5636462bb
ted <ted@warpedpuppy.com>
2020-11-24 11:39:15 -0500

    fixed movie layout

--- src/App.js
+++ src/App.js
@@ -23,7 +23,8 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
-        <div className="movie-reel"></div>
+        {/* <div className="movie-reel"></div> */}
+        {/* <img src="filmStrip.png" /> */}
         <Header />
           <main>
           <Switch>
